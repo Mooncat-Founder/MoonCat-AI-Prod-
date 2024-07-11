36f9035d57ebe0c3fec2202f2365fec9b77aaf03
Mooncat-Founder <mooncatai@outlook.com>
2024-07-11 19:48:03 +1000

    fixed some stuff

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,10 @@
-// Import the MoonCatToken contract artifact
-const MoonCatToken = artifacts.require("MoonCatToken");
+const MoonTestToken = artifacts.require("MoonTestToken");
 
 module.exports = function(deployer) {
     // Specify the initial supply of tokens (e.g., 10000 tokens, with 18 decimals)
     // The `web3.utils.toWei` function converts the amount into the smallest unit (wei)
     // Since ERC20 tokens use 18 decimals by default, we treat 1 token as 1 * 10^18 wei
-    const initialSupply = web3.utils.toWei('10000', 'ether'); // 10,000 MoonCatToken tokens
+    const initialSupply = web3.utils.toWei('10000000', 'ether'); 
 
-    // Deploy the MoonCatToken contract with the specified initial supply
-    deployer.deploy(MoonCatToken, initialSupply);
+    deployer.deploy(MoonTestToken, initialSupply);
 };
