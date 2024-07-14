c86d8b13a268965171fee93497ec51946ffca912
Mooncat-Founder <mooncatai@outlook.com>
2024-07-14 15:31:27 +1000

    final check in for final security changes.

--- contracts/moontesttoken.sol
+++ contracts/moontesttoken.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract VariableToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+contract MoonTestToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("MoonTestToken", "MTT") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
