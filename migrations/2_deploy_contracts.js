ff3a983ce71f79784a8cfaa0e80da029e26a4261
Mooncat-Founder <mooncatai@outlook.com>
2024-07-14 15:52:56 +1000

    Revert "final check in for final security changes."
    This reverts commit c86d8b13a268965171fee93497ec51946ffca912.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ export default function(deployer) {
     // Retrieve environment variables
     const name = process.env.TOKEN_NAME;
     const symbol = process.env.TOKEN_SYMBOL;
-    const initialSupply = web3.utils.toWei('1000000000', 'ether'); // Example initial supply
+    const initialSupply = web3.utils.toWei('10000', 'ether'); // Example initial supply
 
     // Deploy the contract with the specified name, symbol, and initial supply
     deployer.deploy(VariableToken, name, symbol, initialSupply);
