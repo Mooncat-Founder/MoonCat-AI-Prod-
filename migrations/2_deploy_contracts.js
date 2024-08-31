bf4514c09618719fb1bf4c55e28362a2b1291aad
Mooncat-Founder <mooncatai@outlook.com>
2024-08-31 15:39:52 +1000

    checked in after build

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Alchemy, Network } = require('alchemy-sdk');
+const fs = require('fs');
+const path = require('path');
 const MoontestToken = artifacts.require("MoontestToken");
 const Staking = artifacts.require("Staking");
 
@@ -29,13 +31,39 @@ const provider = new HDWalletProvider({
 
 module.exports = async function(deployer) {
   try {
+    // Deploy the MoontestToken contract
     await deployer.deploy(MoontestToken, tokenName, tokenSymbol, tokenInitialSupply);
     const tokenInstance = await MoontestToken.deployed();
     console.log("MoontestToken deployed at:", tokenInstance.address);
 
+    // Deploy the Staking contract
     await deployer.deploy(Staking, tokenInstance.address);
     const stakingInstance = await Staking.deployed();
     console.log("Staking deployed at:", stakingInstance.address);
+
+    // Prepare the contract data to be saved
+    const deploymentData = {
+      MoontestToken: {
+        address: tokenInstance.address,
+        abi: tokenInstance.abi
+      },
+      Staking: {
+        address: stakingInstance.address,
+        abi: stakingInstance.abi
+      }
+    };
+
+    // Ensure the 'deployed' directory exists
+    const directoryPath = path.resolve(__dirname, 'deployed');
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    // Save the contract data to a file
+    const filePath = path.resolve(directoryPath, `MoontestTokenAndStaking-${deployer.network}.json`);
+    fs.writeFileSync(filePath, JSON.stringify(deploymentData, null, 2));
+
+    console.log(`Contract ABI and Address saved to ${filePath}`);
   } catch (error) {
     console.error("Deployment failed:", error);
   } finally {
